63fa881b4af33e45b6805cadf22756835de3b4f3
lggaray <lgaray887@famaf.unc.edu.ar>
2021-06-24 13:50:27 +0800

    Add ffmpeg to the pipeline

--- change_videos_name.py
+++ change_videos_name.py
@@ -2,9 +2,10 @@ import os
 from shutil import move
 
 path1 = 'RAWvideos/'
-path2 = 'RESvideos/'
-files = os.listdir(path1)
+path2 = 'NAMvideos/'
+path3 = 'RESvideos/'
 
+files = os.listdir(path1)
 for file in files:
     file_name = file.split('、')[0]
     inner_files = os.listdir(path1+file)
@@ -18,4 +19,8 @@ for file in files:
         move(path1+file+'/'+left_video[0], path2+file_name+'_left.mov')
     except:
         print('no left video')
-       
+     
+files = os.listdir(path2)
+for file in files:
+    file_name = file.split('.')[0]
+    os.system('ffmpeg -i {} -preset fast -codec:a libfdk_aac -b:a 128k -codec:v libx264 -pix_fmt yuv420p -b:v 750k -minrate 400k -maxrate 1000k -bufsize 1500k -vf scale=-1:360 {}.mp4'.format(path2+file, path3+file_name))
